ff6d16f0b81cb0afaf2e54310c9b916aa98c8737
Vlad Korobko <max.vlad.kor@gmail.com>
2023-05-13 00:15:38 +0300

    Began my first next.js app by YouTube tutorial

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import '@styles/globals.css';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+    title: 'Promtopia',
+    description: 'Discover & Share AI  Prompts'
+}
+
+const Root = ({children}) => {
+    return (
+        <html lang="en">
+            <body>
+                <div className="main">
+                    <div className="gradient" />
+                </div>
+
+                <main className="app">
+                    <Nav />
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
+};
+
+export default Root;
