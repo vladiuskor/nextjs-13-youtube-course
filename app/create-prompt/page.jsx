1192111e321208a9cfe2a79603fbd8694b7d0247
Vlad Korobko <max.vlad.kor@gmail.com>
2023-05-15 18:28:03 +0300

    Added page for creating prompts and it functionality (API, DB)

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,6 +7,9 @@ import {useRouter} from  'next/navigation';
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
+    const router = useRouter();
+    const {data: session} = useSession();
+
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
@@ -14,7 +17,28 @@ const CreatePrompt = () => {
     });
 
     const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
 
+        try {
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    tag: post.tag,
+                    userId: session?.user.id
+                })
+            });
+
+            if(response.ok) {
+                router.push('/');
+            }
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setSubmitting(false);
+        }
     }
 
     return (
